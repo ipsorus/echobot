f3cd59c10e8ddfef876f57fa12e4c844171e49c0
Loshkarev Vadim <ipsorus@inbox.ru>
2019-09-15 12:02:58 +0300

    Edit bot Ephem

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 PROXY = {'proxy_url': 'socks5://t3.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
+PLANETS = ['Mars', 'Venus', 'Saturn', 'Pluto', 'Mercury', 'Uranus', 'Jupiter']
+
 import ephem
 import datetime
 
@@ -14,27 +16,31 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 def greet_user(bot, update):
     text = 'Вызван /start'
-    about = 'Чтобы увидеть \nв каком созвездии находится \nнужная Вам планета \nв настоящее время, \nВы можете выполнить команду \n/planet + \'Название планеты\''
+    bot_functions = 'Функции бота:'
+    about = '/planet - узнать созвездие планеты'
     print(text)
+    print(bot_functions)
     print(about)
     update.message.reply_text(text)
+    update.message.reply_text(bot_functions)
     update.message.reply_text(about)
 
 def talk_to_me(bot, update):
-    user_text = update.message.text 
+    user_text = update.message.text
     print(user_text)
     update.message.reply_text(user_text)
 
-#def select_planet(bot, update):
-    #text = 'Выбери планету:\n/Mars\n/Earth\n/Venus\n/Saturn\n/Pluto\n/Mercury\n/Uranus\n/Jupiter'
-    #print(text)
-    #update.message.reply_text(text)
+def select_planet(bot, update):
+    text = 'Выбери планету:\n/Mars\n/Venus\n/Saturn\n/Pluto\n/Mercury\n/Uranus\n/Jupiter'
+    print(text)
+    update.message.reply_text(text)
 
-def planet_info(bot, update):
-    user_planet = update.message.text.split()
-    list_planet = ['Mars', 'Earth', 'Venus', 'Saturn', 'Pluto', 'Mercury', 'Uranus', 'Jupiter']
+'''def planet_info(bot, update):
+    user_planet = update.message.text.strip('/')
+    print(user_planet)
+    list_planet = ['Mars', 'Venus', 'Saturn', 'Pluto', 'Mercury', 'Uranus', 'Jupiter']
     realtime_date = datetime.datetime.now().strftime("%Y/%m/%d")
-    if user_planet[1].lower().capitalize() in list_planet:
+    if user_planet.lower().capitalize() in list_planet:
         planet = getattr(ephem, user_planet[1].lower().capitalize())
         planet = planet(realtime_date)
         const = ephem.constellation(planet)
@@ -44,9 +50,22 @@ def planet_info(bot, update):
     else:
         res = 'Ошибка! Попробуйте ввести название Mars, Earth, Venus, Saturn, Pluto, Mercury, Uranus, Jupiter'
         print(res)
-        update.message.reply_text(res)
+        update.message.reply_text(res)'''
+def planet_info(bot, update):
+    user_planet = update.message.text.strip('/')
+    #list_planet = ['Mars', 'Venus', 'Saturn', 'Pluto', 'Mercury', 'Uranus', 'Jupiter']
+    realtime_date = datetime.datetime.now().strftime("%Y/%m/%d")
 
-    
+    planet = getattr(ephem, user_planet)
+    planet = planet(realtime_date)
+    const = ephem.constellation(planet)
+    res = f'Планета {user_planet} {realtime_date} находится в созвездии {const[1]}'
+    print(res)
+    update.message.reply_text(res)
+    #else:
+        #res = 'Ошибка! Попробуйте ввести название Mars, Earth, Venus, Saturn, Pluto, Mercury, Uranus, Jupiter'
+        #print(res)
+        #update.message.reply_text(res)
 
 def main():
     mybot = Updater('886652216:AAEs_2UulyBgeNc_YgwbjpLf4BI0PIIN00Y', request_kwargs=PROXY)
@@ -54,8 +73,8 @@ def main():
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    #dp.add_handler(CommandHandler('planet', select_planet))
-    dp.add_handler(CommandHandler('planet', planet_info))
+    dp.add_handler(CommandHandler('planet', select_planet))
+    dp.add_handler(CommandHandler(PLANETS, planet_info))
    
     mybot.start_polling()
     mybot.idle()
