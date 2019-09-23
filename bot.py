eef8e50a1c7f4dacc1db964ca09a15445bc363ab
Loshkarev Vadim <ipsorus@inbox.ru>
2019-09-23 22:25:10 +0300

    Add full_moon func

--- bot.py
+++ bot.py
@@ -4,9 +4,13 @@ PROXY = {'proxy_url': 'socks5://t3.learn.python.ru:1080',
     'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
 PLANETS = ['Mars', 'Venus', 'Saturn', 'Pluto', 'Mercury', 'Uranus', 'Jupiter']
+CITIES = ['Москва','Альметьевск', 'Уфа', 'Хабаровск', 'Томск', 'Екатеринбург', 'Оренбург', 'Кемерово', 'Грозный', 'Воскресенск', 'Рязань', 'Новосибирск', 'Калуга', 'Брянск', 'Кострома', 'Алушта', 'Орел', 'Киров', 'Владимир', 'Ряжск', 'Курск', 'Коломна', 'Абакан', 'Тула', 'Нью-Йорк', 'Вашингтон']
 
 import ephem
-import datetime
+import locale
+locale.setlocale(locale.LC_TIME, 'ru_RU')
+
+from datetime import date, timedelta, datetime
 
 import logging
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -15,15 +19,21 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     )
 
 def greet_user(bot, update):
-    text = 'Вызван /start'
+    text = 'Добро пожаловать, это тестовый информационный бот.'
     bot_functions = 'Функции бота:'
     about = '/planet - узнать созвездие планеты'
+    about_1 = 'введите /wordcount + слова - посчитает количество слов'
+    about_2 = 'введите /next_full_moon + дата (год-месяц-день) - узнать когда ближайшее полнолуние'
     print(text)
     print(bot_functions)
     print(about)
+    print(about_1)
+    print(about_2)
     update.message.reply_text(text)
     update.message.reply_text(bot_functions)
     update.message.reply_text(about)
+    update.message.reply_text(about_1)
+    update.message.reply_text(about_2)
 
 def talk_to_me(bot, update):
     user_text = update.message.text
@@ -55,7 +65,6 @@ def planet_info(bot, update):
     user_planet = update.message.text.strip('/')
     #list_planet = ['Mars', 'Venus', 'Saturn', 'Pluto', 'Mercury', 'Uranus', 'Jupiter']
     realtime_date = datetime.datetime.now().strftime("%Y/%m/%d")
-
     planet = getattr(ephem, user_planet)
     planet = planet(realtime_date)
     const = ephem.constellation(planet)
@@ -67,6 +76,42 @@ def planet_info(bot, update):
         #print(res)
         #update.message.reply_text(res)
 
+def word_count(bot, update):
+    user_words = update.message.text.strip('/wordcount')
+    if user_words != '':
+        user_words_list = user_words.split()
+        res = f'Количество слов в строке: {len(user_words_list)}'
+        print(res)
+        update.message.reply_text(res)
+    else:
+        text = 'Слова не введены'
+        print(text)
+        update.message.reply_text(text)
+
+def next_full_moon(bot, update):
+    user_text = update.message.text.strip('/next_full_moon ')
+    full_moon = ephem.next_full_moon(user_text)
+    res_dt = datetime.strptime(str(full_moon), '%Y/%m/%d %H:%M:%S')
+    res = res_dt.strftime('%A %d %B %Y %H:%M:%S')
+    answ = f'Следующее полнолуние будет: {res}'
+    print(answ)
+    update.message.reply_text(answ)
+
+'''def cities(bot, update):
+    game_city = CITIES
+    deleted_cities = []
+    user_city = update.message.text.strip('/cities ')
+    user_city = user_city.lower().capitalize()
+    if user_city in game_city:
+        for bot_city in 
+        user_city[-1].upper()'''
+
+
+
+
+
+    
+
 def main():
     mybot = Updater('886652216:AAEs_2UulyBgeNc_YgwbjpLf4BI0PIIN00Y', request_kwargs=PROXY)
 
@@ -75,6 +120,10 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     dp.add_handler(CommandHandler('planet', select_planet))
     dp.add_handler(CommandHandler(PLANETS, planet_info))
+    dp.add_handler(CommandHandler('wordcount', word_count))
+    dp.add_handler(CommandHandler('next_full_moon', next_full_moon))
+    #dp.add_handler(CommandHandler('cities', cities))
+
    
     mybot.start_polling()
     mybot.idle()
